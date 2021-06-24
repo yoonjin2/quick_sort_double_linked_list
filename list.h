ca162249f9c538d2ddd615a9a46e66b9bbcd4645
이윤진 <yoonjin2@github.com>
2021-06-24 15:08:41 +0900

    TEST

--- list.h
+++ list.h
@@ -1,5 +1,5 @@
-#define NUM 6
-#define LIM 45
+#define NUM 10000
+#define LIM 10000
 typedef struct node{
 	int key;
 	struct node *prev;
@@ -13,13 +13,22 @@ typedef struct list {
 } list;
 
 void init_list ( list * lst );
-void between  ( list *lst , int item , node * r , node * element ) ;
-int remove_item ( list *lst , node * element ) ;
+void between  ( list * lst , int item , node * r , node * element ) ;
+void push ( list * lst , int item ) ;
+int remove_item ( list * lst , node * element ) ;
+int pop_item ( list * lst ) ;
 int size ( list *lst ) ;
 _Bool empty ( list * lst ) ;
 void show ( list * lst ) ;
 void free_list ( list * lst ) ;
 void empty_list ( list * lst ) ;
-node * find ( list * lst, int key ) ;
+node * find ( list * lst , int key ) ;
+node * index_node ( list * lst , int i ) ;
+/*void merge ( node * a , node * b );
+list * complete ( node * first , node * end );
+node * split ( node * a ) ;
+list split_to_list ( node * end , node * split , int size_n , _Bool is_front ) ;
+*/
 void sort_func ( list * lst ) ;
-void swap ( int * a, int * b );
+void bubble_sort ( list * lst );
+void swap ( int * a, int * b ); 
